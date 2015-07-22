6d2e850a1baa82b73cad6bbb3db6d734d1d02b87
georgej-github <itcl.george@gmail.com>
2015-07-22 07:11:40 +0000

    Update StartApp.sh

--- Scripts/StartApp.sh
+++ Scripts/StartApp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "while true; do { echo -e 'HTTP/1.1 200 OK\r\n'; cat /webapps/Config; } | nc -l 80; done" > /tmp/dummyserver.sh
+echo "while true; do { echo -e 'HTTP/1.1 200 OK\r\n'; cat /webapps/Config/config.txt; } | nc -l 80; done" > /tmp/dummyserver.sh
 
 chmod +x /tmp/dummyserver.sh
 /tmp/dummyserver.sh &
